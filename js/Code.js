a551d60fae8c5e1d0b96ea963c49fec45ae62717
Xhyree09 <84895683+Xhyree09@users.noreply.github.com>
2022-04-16 18:33:50 +0800

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("yellow")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("#CBC3E3")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("yellow")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("#CBC3E3")
+    text("Hint: Performs a particular task !!", 700,170)
+
+    fill("yellow")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("#CBC3E3")
+    text("Hint: Stores all information !!", 100,270)
+
+}
